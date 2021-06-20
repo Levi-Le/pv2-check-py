5bae378896c1983ade15361a66e01cbed0b6443b
Levi V <levi@redhat.com>
2021-06-21 00:10:05 +0200

    comments

--- pv2.py
+++ pv2.py
@@ -2,6 +2,7 @@
 
 import re
 
+######################################################################################
 # color in output
 class bcolors:
     PASS = '\033[92m'
@@ -12,6 +13,7 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
+######################################################################################
 # defining regex
 # abstract_tag = re.compile(r"^\[role\=\"\_abstract\"\]")
 vanilla_xref = re.compile(r'<<.*>>')
@@ -22,6 +24,7 @@ empty_line1 = re.compile(r'\[role=\"_additional-resources\"\](?=\n\n)')
 empty_line = re.compile(r'\[role=\"_additional-resources\"\]\n\n')
 
 
+######################################################################################
 # defining strings
 abstract_tag = '[role="_abstract"]'
 add_res_tag = '[role="_additional-resources"]'
@@ -29,18 +32,22 @@ exp_tag = ':experimental:'
 # TODO: ignore case
 add_res_header = 'Additional resources'
 
+
+######################################################################################
 # recording the files
 filename = "/home/levi/rhel-8-docs/rhel-8/modules/dotnet/con_removed-environment-variables.adoc"
 
 
-
-
+######################################################################################
+#
 def skip_comments(file):
     for line in file:
         if not line.strip().startswith('//'):
             yield line
 
 
+# CHECKS
+######################################################################################
 # abstract tag check
 def abstarct_tag_check(filename):
     # record occurences of abstract tag
@@ -67,11 +74,12 @@ def abstarct_tag_check(filename):
         print(bcolors.FAIL + bcolors.BOLD + "FAIL: abstract tag appears multiple times in the following files:" + bcolors.ENDC, file.name, sep='\n', end='\n')
 
 
+######################################################################################
 # additional resources tag check
 def add_res_tag_check(filename):
     # check if file has related information section
     add_res_wrong_header = re.findall(r'(?<=\=\=\s)Related information', content, re.IGNORECASE) or re.findall(r'(?<=\.)Related information', content, re.IGNORECASE)
-    # if re;ated infor section is used instead of additional resources section => fail msg
+    # if related infor section is used instead of additional resources section => fail msg
     if add_res_wrong_header:
         print(bcolors.FAIL + bcolors.BOLD + "FAIL: 'Related information' section was found. Change the section name to 'Additional resources' in the following files:" + bcolors.ENDC, file.name, sep='\n', end='\n')
     # check if file has additional resources section
@@ -86,15 +94,18 @@ def add_res_tag_check(filename):
         if content.count(add_res_tag) == 1:
             # check if empty line after additional resources tag
             empty_line_after_add_res = re.findall(r'\[role="_additional-resources"]\n(?=\n)', content)
+            # check if empty line after additional resources header
             empty_line_after_add_res_header = re.findall(r'(?<=\=\=\s)Additional resources\n(?=\n)', content, re.IGNORECASE) or (r'(?<=\.)Additional resources\n(?=\n)', content, re.IGNORECASE)
             # if empty line after additional resources tag => fail msg
             if empty_line_after_add_res:
                 print(bcolors.FAIL + bcolors.BOLD + "FAIL: the following files have an empty line after the additional resources tag:" + bcolors.ENDC, file.name, sep='\n', end='\n')
+            # if empty line after additional resources header => fail msg
             if empty_line_after_add_res_header:
                 print(bcolors.FAIL + bcolors.BOLD + "FAIL: the following files have an empty line after the additional resources header:" + bcolors.ENDC, file.name, sep='\n', end='\n')
 
 
-# vanilla xref check
+######################################################################################
+#  vanilla xref check
 def vanilla_check(filename):
     vanilla = re.findall(vanilla_xref, line) and not re.findall(pseudo_vanilla_xref, line)
     if vanilla:
@@ -102,14 +113,17 @@ def vanilla_check(filename):
         # print(line)
 
 
-# open file
+######################################################################################
+#  open file
 with open(filename, "r") as file:
     for line in file:
         line = line.partition('//')[0]
         line = line.rstrip()
         content = file.read()
 
-# report abstract tag check
+# REPORTS
+######################################################################################
+#  report abstract tag check
 abstarct_tag_check(filename)
 
 
