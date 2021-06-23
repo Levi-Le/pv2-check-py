2e4af170629ea8aa983901a7557c32299927bb8e
Levi V <levi@redhat.com>
2021-06-23 20:20:25 +0200

    removes some nesting + useless comments

--- pv2.py
+++ pv2.py
@@ -6,7 +6,6 @@ import os
 filename = "/home/levi/rhel-8-docs/rhel-8/modules/dotnet/con_removed-environment-variables.adoc"
 
 
-# color in output
 class bcolors:
     OK = '\033[92m'
     WARN = '\033[93m'
@@ -66,19 +65,16 @@ def print_warn(message, files):
 
 
 def vanilla_xref_check(stripped_file, file):
-    # check if file has related information section
     if re.findall(regex.VANILLA_XREF, stripped_file):
         print_fail("vanilla xrefs found in the following files", file)
 
 
 def var_in_title_check(stripped_file, file):
-    # check if title has a variable
     if re.findall(regex.VAR_IN_TITLE, stripped_file):
         print_fail("the following files have variable in the level 1 heading", file)
 
 
 def inline_anchor_check(stripped_file, file):
-    # check if file has in-line anchors
     if re.findall(regex.INLINE_ANCHOR, stripped_file):
         print_fail("in-line anchors found in the following files", file)
 
@@ -119,30 +115,23 @@ def human_readable_label_check(stripped_file, file):
 
 
 def abstarct_tag_check(stripped_file, original_file, file):
-    # record occurences of abstract tag
     occurrences_abstract_tag = stripped_file.count(tags.ABSTRACT)
     if occurrences_abstract_tag == 0:
-        # if no abstract tag => fail msg
         print_fail("abstract tag is missing in the following files", file)
         return
     if occurrences_abstract_tag > 1:
         print_fail("abstract tag appears multiple times in the following files", file)
         return
-    # check if abstract tag is set once
-    if occurrences_abstract_tag == 1:
-        first_paragraph_check = re.findall(regex.FIRST_PARA, original_file)
-        if first_paragraph_check:
-            print_fail("there is no line between the level 1 heading and the abstract tag in the following files. the first paragraph might render incorrectly", file)
-            return
-        empty_line_before_abstract_tag = re.findall(regex.NO_EMPTY_LINE_BEFORE_ABSTRACT, original_file)
-        if empty_line_before_abstract_tag:
-            print_fail("the following files have no empty line before the abstract tag", file)
-        empty_line_after_abstract_tag = re.findall(regex.EMPTY_LINE_AFTER_ABSTRACT, original_file)
-        if empty_line_after_abstract_tag:
-            print_fail("the following files have an empty line after the abstract tag", file)
-            return
-        comment_after_abst_tag = re.findall(regex.COMMENT_AFTER_ABSTRACT, original_file)
-        if comment_after_abst_tag:
+    # if the abstract tag is only set once:
+    if re.findall(regex.FIRST_PARA, original_file):
+        print_fail("there is no line between the level 1 heading and the abstract tag in the following files. the first paragraph might render incorrectly", file)
+        return
+    if re.findall(regex.NO_EMPTY_LINE_BEFORE_ABSTRACT, original_file):
+        print_fail("the following files have no empty line before the abstract tag", file)
+    if re.findall(regex.EMPTY_LINE_AFTER_ABSTRACT, original_file):
+        print_fail("the following files have an empty line after the abstract tag", file)
+        return
+    if re.findall(regex.COMMENT_AFTER_ABSTRACT, original_file):
             print_fail("the following files have an comment after the abstract tag", file)
 
 
@@ -150,38 +139,37 @@ def add_res_section_check(stripped_file, original_file, file):
     if re.findall(regex.RELATED_INFO, stripped_file):
         print_fail("'Related information' section was found in the following files. Change the section name to 'Additional resources'", file)
         return
-    if re.findall(regex.ADDITIONAL_RES, stripped_file):
-        name_of_file = os.path.basename(file)
-        if file_types.ASSEMBLY.fullmatch(name_of_file):
-            if not re.findall(regex.ADD_RES_ASSEMBLY, stripped_file):
-                print_fail("additional resources section for assemblies should be `== Additional resources`", file)
+    if not re.findall(regex.ADDITIONAL_RES, stripped_file):
+        return
+    # if the file has additional resources section:
+    name_of_file = os.path.basename(file)
+    if file_types.ASSEMBLY.fullmatch(name_of_file):
+        if not re.findall(regex.ADD_RES_ASSEMBLY, stripped_file):
+            print_fail("additional resources section for assemblies should be `== Additional resources`", file)
         elif not re.findall(regex.ADD_RES_MODULE, stripped_file):
-                print_fail("additional resources section for modules should be `.Additional resources`", file)
-        if stripped_file.count(tags.ADD_RES) == 0:
-            print_fail("additional resources tag is missing in the found in the following files", file)
-            return
-        if stripped_file.count(tags.ADD_RES) > 1:
-            print_fail("additional resources tag appears multiple times in the following files", file)
-        if stripped_file.count(tags.ADD_RES) == 1:
-            if re.findall(regex.EMPTY_LINE_AFTER_ADD_RES_TAG, original_file):
-                print_fail("the following files have an empty line after the additional resources tag", file)
-            elif re.findall(regex.COMMENT_AFTER_ADD_RES_TAG, original_file):
-                print_fail("the following files have comments after the additional resources tag", file)
-            if re.findall(regex.EMPTY_LINE_AFTER_ADD_RES_HEADER, original_file):
-                print_fail("the following files have an empty line after the additional resources header", file)
-            elif re.findall(regex.COMMENT_AFTER_ADD_RES_HEADER, original_file):
-                    print_fail("the following files have comments after the additional resources header", file)
+            print_fail("additional resources section for modules should be `.Additional resources`", file)
+    if stripped_file.count(tags.ADD_RES) == 0:
+        print_fail("additional resources tag is missing in the found in the following files", file)
+        return
+    if stripped_file.count(tags.ADD_RES) > 1:
+        print_fail("additional resources tag appears multiple times in the following files", file)
+    if stripped_file.count(tags.ADD_RES) == 1:
+        if re.findall(regex.EMPTY_LINE_AFTER_ADD_RES_TAG, original_file):
+            print_fail("the following files have an empty line after the additional resources tag", file)
+        elif re.findall(regex.COMMENT_AFTER_ADD_RES_TAG, original_file):
+            print_fail("the following files have comments after the additional resources tag", file)
+        if re.findall(regex.EMPTY_LINE_AFTER_ADD_RES_HEADER, original_file):
+            print_fail("the following files have an empty line after the additional resources header", file)
+        elif re.findall(regex.COMMENT_AFTER_ADD_RES_HEADER, original_file):
+                print_fail("the following files have comments after the additional resources header", file)
 
 
 def validation(filename):
     with open(filename, "r") as file:
         original = file.read()
-        # exclude multi-line comments
         stripped = regex.MULTI_LINE_COMMENT.sub('', original)
-        # exclude single-line comments
         stripped = regex.SINGLE_LINE_COMMENT.sub('', stripped)
         # FIXME:
-        # excludes pseudo xrefs
         stripped = regex.PSEUDO_VANILLA_XREF.sub('', stripped)
         stripped = regex.CODE_BLOCK.sub('', stripped)
 
